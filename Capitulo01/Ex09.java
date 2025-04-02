ab57a591fe123833635a4f058a6b1e5d4c77be3b
Warlen Sousa <warlen.asousa1@gmail.com>
2025-04-02 00:58:53 -0300

    Adicionado exercício 10 e colocando exercício 09 na pasta certa!

---
+++ Capitulo01/Ex09.java
@@ -0,0 +1,13 @@
+package Capitulo01;
+public class Ex09 {
+    //  Exercício 9: Tipos de Variáveis
+    static String  variavelGlobal  = "Naruto";
+
+    public static void main(String[] args) {
+        String  variavelLocal  = "Pokémon";
+
+        System.out.println("Global: " + variavelGlobal);
+        System.out.println("Local: " + variavelLocal);
+        
+    }
+}
---
+++ Capitulo01/Ex10.java
@@ -0,0 +1,19 @@
+package Capitulo01;
+
+import java.util.Scanner;
+
+public class Ex10 {
+    public static void main(String[] args) {
+        //  Exercício 10: Formatação de Saída
+        
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Digite um número decimal com duas casas decimais: ");
+            double valor = sc.nextDouble();
+            String.format("%.2f", valor);
+            System.out.println(valor);
+            
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage()); 
+        }
+    }
+}
