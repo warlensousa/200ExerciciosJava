8dbb887f366115fc5ee9cbe41af2be3ba07a1fa5
Warlen Sousa <warlen.asousa1@gmail.com>
2025-04-02 00:41:48 -0300

    Adicionando exercício 09

---
+++ Ex09.java
@@ -0,0 +1,12 @@
+public class Ex09 {
+    //  Exercício 9: Tipos de Variáveis
+    static String  variavelGlobal  = "Naruto";
+
+    public static void main(String[] args) {
+        String  variavelLocal  = "Pokémon";
+
+        System.out.println("Global: " + variavelGlobal);
+        System.out.println("Local: " + variavelLocal);
+        
+    }
+}
